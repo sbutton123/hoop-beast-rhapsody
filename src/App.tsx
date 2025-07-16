12952773ed6eb807ea5e17829575141732e33b0b
sbutton123 <shandahulahooping@gmail.com>
2025-07-16 15:50:21 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 // src/App.tsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Navbar from './components/Navbar';
+// global navbar
+import Navbar from '@/components/Navbar'
 
-// import all of your pages:
-import Index from './pages/Index';
-import Tutorials from './pages/Tutorials';
-import Showcase from './pages/Showcase';
-import Programs from './pages/Programs';
-import Workouts from './pages/Workouts';
-import About from './pages/About';
-import Products from './pages/Products';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+// all your pages
+import Index from './pages/Index'
+import Tutorials from './pages/Tutorials'
+import Showcase from './pages/Showcase'
+import Programs from './pages/Programs'
+import Workouts from './pages/Workouts'
+import About from './pages/About'
+import Products from './pages/Products'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
-      {/* nav sits above everything */}
+      {/* ← Navbar sits here once for every route */}
       <Navbar />
 
-      {/* your page content */}
-      <main className="pt-16"> {/* pt-16 to push it below the sticky navbar */}
+      {/* push page content down below the sticky navbar */}
+      <main className="pt-16">
         <Routes>
+          {/* restore home as the root route */}
           <Route path="/" element={<Index />} />
           <Route path="/tutorials" element={<Tutorials />} />
           <Route path="/showcase" element={<Showcase />} />
@@ -32,12 +34,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="/contact" element={<Contact />} />
-          {/* catch-all */}
+
+          {/* catch‑all 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
