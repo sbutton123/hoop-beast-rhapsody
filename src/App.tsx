31bb18da8525b0c34b66a7318c6bb3abc8badb3e
sbutton123 <shandahulahooping@gmail.com>
2025-07-16 15:11:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Index from "./pages/Index";
-import Tutorials from "./pages/Tutorials";
-import Showcase from "./pages/Showcase";
-import Programs from "./pages/Programs";
-import Workouts from "./pages/Workouts";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import NotFound from "./pages/NotFound";
+// src/App.tsx
 
-const queryClient = new QueryClient();
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Index />} />
-            <Route path="tutorials" element={<Tutorials />} />
-            <Route path="showcase" element={<Showcase />} />
-            <Route path="programs" element={<Programs />} />
-            <Route path="workouts" element={<Workouts />} />
-            <Route path="about" element={<About />} />
-            <Route path="products" element={<Products />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+import Layout   from '@/Layout'
+import Index    from '@/pages/Index'
+import Tutorials from '@/pages/Tutorials'
+import Showcase from '@/pages/Showcase'
+import Programs from '@/pages/Programs'
+import Workouts from '@/pages/Workouts'
+import About    from '@/pages/About'
+import Products from '@/pages/Products'
+import Contact  from '@/pages/Contact'
+import NotFound from '@/pages/NotFound'
 
-export default App;
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* home */}
+          <Route index element={<Index />} />
+
+          {/* site pages */}
+          <Route path="tutorials" element={<Tutorials />} />
+          <Route path="showcase"  element={<Showcase  />} />
+          <Route path="programs"  element={<Programs />} />
+          <Route path="workouts"  element={<Workouts />} />
+          <Route path="about"     element={<About    />} />
+          <Route path="products"  element={<Products />} />
+
+          {/* new contact page */}
+          <Route path="contact"   element={<Contact  />} />
+        </Route>
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
