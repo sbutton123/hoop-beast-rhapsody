747de42084eff9864b30d6f34fba1bfba7e5abf1
sbutton123 <shandahulahooping@gmail.com>
2025-07-16 15:33:58 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import Tutorials from '@/pages/Tutorials';
-import Showcase from '@/pages/Showcase';
-import Programs from '@/pages/Programs';
-import Workouts from '@/pages/Workouts';
-import About from '@/pages/About';
-import Products from '@/pages/Products';
-import Contact from '@/pages/Contact'; // new page
+import Navbar from './components/Navbar';
 
-const App = () => {
+// import all of your pages:
+import Index from './pages/Index';
+import Tutorials from './pages/Tutorials';
+import Showcase from './pages/Showcase';
+import Programs from './pages/Programs';
+import Workouts from './pages/Workouts';
+import About from './pages/About';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      {/* nav sits above everything */}
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/tutorials" element={<Tutorials />} />
-        <Route path="/showcase" element={<Showcase />} />
-        <Route path="/programs" element={<Programs />} />
-        <Route path="/workouts" element={<Workouts />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* 404 fallback could go here */}
-      </Routes>
-    </Router>
+      {/* your page content */}
+      <main className="pt-16"> {/* pt-16 to push it below the sticky navbar */}
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+          <Route path="/showcase" element={<Showcase />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/workouts" element={<Workouts />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* catch-all */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
